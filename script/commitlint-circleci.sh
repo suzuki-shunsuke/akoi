0f591944687195730cca1d68d98b707743c57262
Suzuki Shunsuke <suzuki.shunsuke.1989@gmail.com>
2018-07-23 00:08:02 +0900

    docs: add status badges to README

--- script/commitlint-circleci.sh
+++ script/commitlint-circleci.sh
@@ -1,5 +1,5 @@
 if [ -n "$CIRCLE_PULL_REQUEST" ]; then
   npx commitlint --from master --to $CIRCLE_BRANCH || exit 1
 else
-  npx commitlint --from HEAD~10 --to HEAD || exit 1
+  npx commitlint --from HEAD~3 --to HEAD || exit 1
 fi
