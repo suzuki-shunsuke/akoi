3c76d33037ab9f5492434d4fbe05b8c497f87a75
Suzuki Shunsuke <suzuki.shunsuke.1989@gmail.com>
2018-07-28 17:17:38 +0900

    refactor: move cli.Command to command package

--- command/init.go
+++ command/init.go
@@ -8,6 +8,20 @@ import (
 	"github.com/suzuki-shunsuke/akoi/usecase"
 )
 
+// InitCommand is the sub command "init".
+var InitCommand = cli.Command{
+	Name:   "init",
+	Usage:  "create a configuration file if it doesn't exist",
+	Action: Init,
+	Flags: []cli.Flag{
+		cli.StringFlag{
+			Name:  "dest, d",
+			Usage: "created configuration file path",
+			Value: "/etc/akoi/akoi.yml",
+		},
+	},
+}
+
 // Init is the sub command "init".
 func Init(c *cli.Context) error {
 	err := usecase.InitConfigFile(
