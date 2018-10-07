8e713dfb3f15f0d994d206ba5f37b9a1528cd28c
Suzuki Shunsuke <suzuki.shunsuke.1989@gmail.com>
2018-10-07 18:48:37 +0900

    ci: test release on ci

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,6 +1,11 @@
+git status --porcelain || exit 1
+# mainly ignore change of package-lock.json
+git checkout -- . || exit 1
 if [ -n "$CIRCLE_TAG" ]; then
-  git status --porcelain || exit 1
-  # mainly ignore change of package-lock.json
-  git checkout -- . || exit 1
-  curl -sL https://git.io/goreleaser | bash || exit 1
+  goreleaser release || exit 1
+else
+  TAG=`git tag | tail -n 1`-alpha
+  git tag $TAG || exit 1
+  goreleaser release --skip-publish || exit 1
+  git tag -d $TAG || exit 1
 fi
