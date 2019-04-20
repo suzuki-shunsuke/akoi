8193ecb87f25cc317d9b95fb2a0b61b8a4f4c0d7
Suzuki Shunsuke <suzuki.shunsuke.1989@gmail.com>
2019-04-20 11:18:52 +0900

    test: add test

--- internal/usecase/create_link_test.go
+++ internal/usecase/create_link_test.go
@@ -78,8 +78,55 @@ func TestLogicCreateLink(t *testing.T) {
 	for _, d := range data {
 		t.Run(d.title, func(t *testing.T) {
 			logic := newLogicMock(t)
-			logic.Fsys = d.fsys
+			if d.fsys != nil {
+				logic.Fsys = d.fsys
+			}
 			file, err := logic.CreateLink(d.file)
+			if d.isErr {
+				require.NotNil(t, err)
+				return
+			}
+			require.Nil(t, err)
+			require.Equal(t, d.exp, file)
+		})
+	}
+}
+
+func TestLogicRemoveFileAndCreateLink(t *testing.T) {
+	data := []struct {
+		title string
+		file  domain.File
+		fsys  domain.FileSystem
+		isErr bool
+		exp   domain.File
+	}{
+		{
+			title: "failed to remove a file",
+			file:  domain.File{Result: &domain.FileResult{}},
+			fsys: test.NewFileSystem(t, gomic.DoNothing).
+				SetReturnRemoveFile(fmt.Errorf("permission denied")),
+			isErr: true,
+		}, {
+			title: "failed to create a link",
+			file:  domain.File{Result: &domain.FileResult{}},
+			fsys: test.NewFileSystem(t, gomic.DoNothing).
+				SetReturnMkLink(fmt.Errorf("failed to create a link")),
+			isErr: true,
+		}, {
+			title: "normal",
+			file:  domain.File{Result: &domain.FileResult{}},
+			exp: domain.File{Result: &domain.FileResult{
+				FileRemoved: true,
+				Migrated:    true}},
+		},
+	}
+	for _, d := range data {
+		t.Run(d.title, func(t *testing.T) {
+			logic := newLogicMock(t)
+			if d.fsys != nil {
+				logic.Fsys = d.fsys
+			}
+			file, err := logic.RemoveFileAndCreateLink(d.file)
 			if d.isErr {
 				require.NotNil(t, err)
 				return
