3787f873bf3a1bbd06e8b2e03ac9da07a28fcee3
Suzuki Shunsuke <suzuki.shunsuke.1989@gmail.com>
2019-04-20 12:06:57 +0900

    test: add test

--- internal/usecase/setup_config_test.go
+++ internal/usecase/setup_config_test.go
@@ -36,3 +36,52 @@ func Test_logicSetupConfig(t *testing.T) {
 	_, err := logic.SetupConfig(cfg)
 	require.Nil(t, err)
 }
+
+func Test_logicSetupPkgConfig(t *testing.T) {
+	pkg := domain.Package{
+		RawURL:  "https://releases.hashicorp.com/consul/{{.Version}}/consul_{{.Version}}_darwin_amd64.zip",
+		Version: "1.2.1",
+		Files: []domain.File{
+			{
+				Name:    "consul",
+				Archive: "consul",
+			},
+		},
+	}
+	pkgName := "consul"
+	cfg := domain.Config{
+		BinPath:  "/usr/local/bin/{{.Name}}-{{.Version}}",
+		LinkPath: "/usr/local/bin/{{.Name}}",
+		Packages: map[string]domain.Package{
+			pkgName: pkg,
+		},
+	}
+	logic := newLogicMock(t)
+	logic.Fsys = test.NewFileSystem(t, gomic.DoNothing).
+		SetFuncExpandEnv(func(p string) string {
+			return p
+		})
+	_, err := logic.SetupPkgConfig(cfg, pkgName, pkg, 4)
+	require.Nil(t, err)
+}
+
+func Test_logicSetupFileConfig(t *testing.T) {
+	file := domain.File{
+		Name:     "consul",
+		Archive:  "consul",
+		BinPath:  "/usr/local/bin/akoi-{{.Version}}",
+		LinkPath: "/usr/local/bin/akoi",
+	}
+	pkg := domain.Package{
+		RawURL:  "https://releases.hashicorp.com/consul/{{.Version}}/consul_{{.Version}}_darwin_amd64.zip",
+		Version: "1.2.1",
+		Files:   []domain.File{file},
+	}
+	logic := newLogicMock(t)
+	logic.Fsys = test.NewFileSystem(t, gomic.DoNothing).
+		SetFuncExpandEnv(func(p string) string {
+			return p
+		})
+	_, err := logic.SetupFileConfig(pkg, file)
+	require.Nil(t, err)
+}
